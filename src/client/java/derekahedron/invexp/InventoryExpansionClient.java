7e06476f2ebffb7413180f25fed5016441b43703
Derekahedron <derekahedron@gmail.com>
2025-01-04 17:54:41 -0500

    Removed import statement

--- src/client/java/derekahedron/invexp/InventoryExpansionClient.java
+++ src/client/java/derekahedron/invexp/InventoryExpansionClient.java
@@ -4,7 +4,6 @@ import derekahedron.invexp.render.item.model.InvExpItemModels;
 import derekahedron.invexp.render.item.property.InvExpItemProperties;
 import derekahedron.invexp.screen.InvExpScreenEvents;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.input.Scroller;
 
 /**
  * Handles client-only initialization for Inventory Expansion.
